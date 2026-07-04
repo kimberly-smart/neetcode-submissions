74c9a2ca82e14b90a4d15cda0e04ac9863cceed9
Kim Smart <kimeunbismart@gmail.com>
2026-07-04 14:01:48 -0400

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        res = 0
+        buy = prices[0]
+
+        for sell in prices:
+            if sell < buy:
+                buy = sell
+            
+            res = max(res, sell - buy)
+        
+        return res
